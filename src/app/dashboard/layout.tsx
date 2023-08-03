42bc8e76ee894ace54b65cc6e6e68d28f32637af
unknown <ryan20bv@gmail.com>
2023-08-03 18:39:45 +0800

    added layout nav for dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DashboardNav from "../components/nav/DashboardNav";
 import SideBar from "../components/ui/SideBar";
 
 export const metadata = {
@@ -11,11 +12,11 @@ interface PropsType {
 
 const DashboardLayout: React.FC<PropsType> = ({ children }) => {
 	return (
-		<main className='h-screen border border-black'>
-			<section className='absolute h-[80%] w-[200px]'>
-				<SideBar />
-			</section>
-			<section className='text-center'>{children}</section>
+		<main className='h-screen overflow-x-scroll'>
+			<DashboardNav />
+			<SideBar />
+
+			{children}
 		</main>
 	);
 };
